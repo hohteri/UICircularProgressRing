6b162ee6483b0536fbabc7657797ef0d3e6d90d5
Harri Hohteri <harri.hohteri@sportiq.xyz>
2020-11-22 09:11:42 +0200

    Add spm support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "UICircularProgressRing",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "UICircularProgressRing",
+            targets: ["UICircularProgressRing"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "UICircularProgressRing",
+            dependencies: [],
+						path: "src/UICircularProgressRing"),
+    ]
+)
